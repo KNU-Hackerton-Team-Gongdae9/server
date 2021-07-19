d59d955ebe0d5289c7d0ff5376dc40846a9d6907
Chang Muk Kim <57346393+kcm0147@users.noreply.github.com>
2021-07-19 20:50:22 +0900

    FEAT: ADD Member domain

---
+++ src/main/java/com/knu/community/email/service/MemberDetailsService.java
@@ -0,0 +1,27 @@
+package com.knu.community.email.service;
+
+import com.knu.community.member.domain.Member;
+import com.knu.community.member.repository.MemberRepository;
+import com.knu.community.member.security.SecurityMember;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberDetailsService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        Member member = memberRepository.findByEmail(email);
+        if(email == null){
+            throw new UsernameNotFoundException(email + " : 사용자 존재하지 않음");
+        }
+
+        return new SecurityMember(member);
+    }
+}

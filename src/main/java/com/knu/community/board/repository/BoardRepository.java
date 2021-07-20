2d04dde136801a4ac4d718bdbdcdb1e836608b8f
nowwater <6360879@naver.com>
2021-07-20 23:35:27 +0900

    FEAT: 게시판 조회 시 포함된 댓글이랑 답글 조회

--- src/main/java/com/knu/community/board/repository/BoardRepository.java
+++ src/main/java/com/knu/community/board/repository/BoardRepository.java
@@ -4,6 +4,7 @@ import com.knu.community.board.domain.Board;
 import com.knu.community.board.domain.Category;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board,Long> {
@@ -13,4 +14,7 @@ public interface BoardRepository extends JpaRepository<Board,Long> {
     Optional<List<Board>> findByCategory(Category category);
 
     Optional<List<Board>> findByAuthor(String author);
+
+    @EntityGraph(attributePaths = {"commentList", "commentList.replyList"})
+    Board findBoardWithAllById(Long id);
 }

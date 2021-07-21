457dfcdd974e03b72edbcc7b9bd7a09ef21be901
hyun.min.han <um4825@gmail.com>
2021-07-21 23:42:45 +0900

    FIX : 닉네임 중복 방지

--- src/main/java/com/knu/community/email/service/AuthService.java
+++ src/main/java/com/knu/community/email/service/AuthService.java
@@ -38,6 +38,9 @@ public class AuthService {
         if(isDuplicateMember(signUpForm.getEmail())){
             throw new IllegalStateException("이미 가입된 회원입니다.");
         }
+        if(isDuplicateNickName(signUpForm.getNickname())){
+            throw new IllegalStateException("닉네임 중복되었습니다. 다른 닉네임으로 해주세요.");
+        }
 
         Member member = Member.builder()
             .email(signUpForm.getEmail())
@@ -52,6 +55,8 @@ public class AuthService {
 
         return memberRepository.save(member);
     }
+
+    private boolean isDuplicateNickName(String nickName) { return memberRepository.existsByNickname(nickName); }
 
     private boolean isDuplicateMember(String email) {
         return memberRepository.existsByEmail(email);
